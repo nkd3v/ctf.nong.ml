18fdbe371307a020c41c8ad47d414c9741b84bfc
64011106 <64011106@kmitl.ac.th>
2022-07-11 22:09:47 +0700

    Set server to use environment port as default

--- server.js
+++ server.js
@@ -19,4 +19,5 @@ app.get('/lonely-doge', (req, res) => {
   res.render('lonely-doge', {visitCount});
 })
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
